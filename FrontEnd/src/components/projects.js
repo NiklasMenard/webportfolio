f2f43063c4a008f573200643c9dd1a7e4a0d5394
Your Name <nmenard@localhost.localdomain>
2020-10-02 10:30:38 +0300

    fixing css

--- FrontEnd/src/components/projects.js
+++ FrontEnd/src/components/projects.js
@@ -3,9 +3,22 @@ import './projects.css'
 
 const Projects = () => {
 
+    const project = [
+        {name:'Project1'},
+        {name:'Project2'},
+        {name:'Project3'},
+        {name:'Project4'},
+        {name:'Project5'},
+        {name:'Project6'},
+    ]
+
     return (
         <div className="project-box">
-                <h1>Projects coming soon</h1>
+            <div className="project-list">
+                <ul>
+                    {project.map(p => <li key={p.name}>{p.name}</li>)}
+                </ul>
+            </div>
         </div>
     )
 }
