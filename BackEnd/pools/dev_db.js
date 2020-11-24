5eb210ece40d4bf30eab288b09f5ef22aefa0982
nmenard <niklas.menard@gmail.com>
2020-11-24 20:42:20 +0200

    fixing config issues

--- BackEnd/pools/dev_db.js
+++ BackEnd/pools/dev_db.js
@@ -1,8 +1,9 @@
+require('dotenv').config('/env');
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  password: '11spinblade',
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PW,
   host: 'localhost',
   port: 5432,
   database: 'clothingwebstoredb',
